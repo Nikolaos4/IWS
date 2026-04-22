514a68145bb8c1375d133e1cd84e9c9155d755a0
Nick Anikanov <nickanikanov12@gmail.com>
2026-04-22 15:48:57 +0300

    added a finish version

--- app/models/voucher_pasport.py
+++ app/models/voucher_pasport.py
@@ -7,7 +7,7 @@ class VoucherPasport(Base):
     __tablename__ = "voucher_pasports"
     voucher_pasport_id = Column(Integer, primary_key=True, index = True)
     purchase_id = Column(Integer, ForeignKey("purchases.purchase_id"), nullable=False)
-    order_id = Column(Integer, ForeignKey("orders.order_id"), nullable=False)
+    order_id = Column(Integer, nullable=True)
 
     purchase = relationship("Purchase", back_populates="voucher_pasport")
     order = relationship("Order", back_populates="voucher_pasport")
